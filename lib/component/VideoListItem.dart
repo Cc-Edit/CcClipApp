aa97a031aad677941ac3c89e8286f033bca59649
ccedit <ccedit@126.com>
2023-12-01 12:46:43 +0800

    列表init

--- lib/component/VideoListItem.dart
+++ lib/component/VideoListItem.dart
@@ -27,27 +27,34 @@ class VideoListItem extends StatelessWidget {
                 0.0, 50 * (1.0 - animation!.value), 0.0),
             child: AspectRatio(
               aspectRatio: 16 / 9,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Image.asset(
-                        listData!.cover,
-                        fit: BoxFit.cover,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                    colors: <Color>[
+                      Color(0xFF333030),
+                      Color(0x563A3733),
+                    ]
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                      color: const Color(0x5644413D)
+                  )
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: Image.asset(
+                          listData!.cover,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        splashColor: Colors.grey.withOpacity(0.2),
-                        borderRadius:
-                        const BorderRadius.all(Radius.circular(4.0)),
-                        onTap: callBack,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
