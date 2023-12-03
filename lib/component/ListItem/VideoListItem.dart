7709a5198f27af0fe957d88dd5f35c87e71e5812
ccedit <ccedit@126.com>
2023-12-03 22:26:22 +0800

    首页列表优化

--- lib/component/ListItem/VideoListItem.dart
+++ lib/component/ListItem/VideoListItem.dart
@@ -94,7 +94,6 @@ class VideoListItem extends StatelessWidget {
                           ],
                         )
                       ),
-
                     ],
                   ),
                 ),
